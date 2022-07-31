cc11f645d62123e5b21c6049cec3239f66826f41
cindee-san <49595063+cindee-san@users.noreply.github.com>
2022-07-31 13:48:14 -0700

    update userSignIn with functional cancel and sign up buttons

--- full_stack_app_with_react_and_a_rest_api-v1/client/src/components/UserSignIn.js
+++ full_stack_app_with_react_and_a_rest_api-v1/client/src/components/UserSignIn.js
@@ -1,39 +1,46 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default class UserSignIn extends Component {
+export default function UserSignIn(){
 
-    render() {
-        return (
-            <body>
-    <div id="root">
-        <header>
-            <div className="wrap header--flex">
-                <h1 className="header--logo"><a href="index.html">Courses</a></h1>
-                <nav>
-                    <ul className="header--signedout">
-                        <li><a href="sign-up.html">Sign Up</a></li>
-                        <li><a href="sign-in.html">Sign In</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+    return (
         <main>
-            <div className="form--centered">
-                <h2>Sign In</h2>
-                
-                <form>
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value=""></input>
-                    <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value=""></input>
-                    <button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
-                </form>
-                <p>Don't have a user account? Click here to <a href="sign-up.html">sign up</a>!</p>
-                
-            </div>
+          <div className="form--centered">
+            <h2>Sign In</h2>
+  
+            <form>
+              <label for="emailAddress">Email Address</label>
+              <input
+                id="emailAddress"
+                name="emailAddress"
+                type="email"
+                value=""
+              ></input>
+              <label for="password">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value=""
+              ></input>
+              <button className="button" type="submit">
+                Sign In
+              </button>
+              <button
+                className="button button-secondary"
+                onclick="event.preventDefault();"
+              >
+               <Link to="/">Cancel</Link> 
+              </button>
+            </form>
+            <p>
+              Don't have a user account? Click here to {" "}
+               <Link to="/signup">sign up</Link>!
+            </p>
+          </div>
         </main>
-    </div>
-</body>
-        );
-    }
+      );
+    
 }
+  
+
